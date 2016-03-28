8505e0a5d9328acc3921004b67112c9bdc8237bb
Ben Roberts <ben@sihnon.net>
2016-03-28 13:32:33 +0100

    Add quick and dirty script for local testing

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -e /vagrant/scripts/functions.sh ]; then
+    echo "Must be run from within the community-buildspec vagrant box"
+    exit 1
+fi
+
+. /vagrant/scripts/functions.sh
+
+if [ ! -e build.yaml ]; then
+    echo "Must be run from a repository directory containing a build.yaml"
+    exit 1
+fi
+
+load_env_from_yaml build.yaml
+
+# Speed up test runs by disabling slow syncs and mirror sorts
+export SKIP_PORTAGE_SYNC=1
+export EQUO_MIRRORSORT=0
+
+# Debug what env vars are being passed to the builder
+printenv
+
+build_all ${BUILD_ARGS}
